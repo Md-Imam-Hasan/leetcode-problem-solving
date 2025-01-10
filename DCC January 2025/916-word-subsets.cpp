713b7c91cf5c2d57a7da8cb88c7531b8039e3bcf
Md-Imam-Hasan <imamhasan0007@gmail.com>
2025-01-10 17:17:58 +0600

    --amend

--- DCC January 2025/916-word-subsets.cpp
+++ DCC January 2025/916-word-subsets.cpp
@@ -5,38 +5,35 @@ using namespace std;
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        int i, j, n1 = words1.size(), n2 = words2.size();
+        int i;
         vector<string> ans;
-        map<char, int> mp;
+        vector<int> maxFreq(26, 0);
 
-        for (i = 0; i < n2; i++) {
-            map<char, int> temp;
-            int len = words2[i].length();
-            string s = words2[i];
-            for (j = 0; j < len; j++) {
-                temp[s[j]]++;
+        for (const auto& word : words2) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                int idx = ch - 'a';
+                freq[idx]++;
+                maxFreq[idx] = max(maxFreq[idx], freq[idx]);
             }
-            for (auto k : temp)
-                mp[k.first] = max(mp[k.first], k.second);
         }
 
-        for (i = 0; i < n1; i++) {
-            bool isSubset = true;
-            map<char, int> temp;
-            int len = words1[i].length();
-            string s = words1[i];
-            for (j = 0; j < len; j++) {
-                temp[s[j]]++;
+        for (const auto& word : words1) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                int idx = ch - 'a';
+                freq[idx]++;
             }
-
-            for (auto k : mp)
-                if (k.second > temp[k.first]) {
+            bool isSubset = true;
+            for (i = 0; i < 26; i++) {
+                if (maxFreq[i] > freq[i]) {
                     isSubset = false;
                     break;
                 }
+            }
 
             if (isSubset)
-                ans.push_back(words1[i]);
+                ans.push_back(word);
         }
 
         return ans;
