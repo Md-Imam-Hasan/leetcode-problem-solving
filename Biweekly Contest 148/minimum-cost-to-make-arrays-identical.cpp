e79f121b2a049703aa38b2ac734ba5a2a30e17a5
Md-Imam-Hasan <imamhasan0007@gmail.com>
2025-01-24 23:57:58 +0600

    refactor(contest): simplify sorting logic in minimum-cost-to-make-arrays-identical.cpp

--- Biweekly Contest 148/minimum-cost-to-make-arrays-identical.cpp
+++ Biweekly Contest 148/minimum-cost-to-make-arrays-identical.cpp
@@ -2,32 +2,8 @@
 
 using namespace std;
 
-// TODO: Not solved yet
-
 class Solution {
 public:
-    vector<int> getOrder(const vector<int>& brr) {
-        vector<int> indices(brr.size());
-        iota(indices.begin(), indices.end(), 0);
-
-        sort(indices.begin(), indices.end(),
-             [&](int i1, int i2) { return brr[i1] < brr[i2]; });
-
-        return indices;
-    }
-
-    void sortByBrr(vector<int>& arr, const vector<int>& brr) {
-        vector<int> sorted_indices = getOrder(brr);
-        vector<int> temp(arr.size());
-
-        sort(arr.begin(), arr.end());
-
-        for (size_t i = 0; i < arr.size(); ++i) {
-            temp[i] = arr[sorted_indices[i]];
-        }
-
-        arr = move(temp);
-    }
     long long minCost(vector<int>& arr, vector<int>& brr, long long k) {
         long long ans = 0, temp = 0, temp2=0;
         int n = arr.size(), i;
@@ -36,9 +12,8 @@ public:
             temp2+=abs(arr[i]-brr[i]);
         }
 
-        sortByBrr(arr, brr);
-
-        for(i=0;i<n;i++) cout<<arr[i]<<"\n";
+        sort(arr.begin(), arr.end());
+        sort(brr.begin(), brr.end());
 
         for (i = 0; i < n; i++) {
             temp += abs(arr[i] - brr[i]);
