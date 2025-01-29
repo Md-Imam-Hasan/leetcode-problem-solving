4a15f55ed5c5e2f4fb0f513b712a63776618c7a7
Md-Imam-Hasan <imamhasan0007@gmail.com>
2025-01-30 00:17:59 +0600

    (DCC Januaryh 2025): add solution for Problem 684 Redundant Connection

---
+++ DCC January 2025/684-redundant-connection.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int find(vector<int>& dsuf, int v) {
+        if (dsuf[v] == -1)
+            return v;
+        return dsuf[v] = find(dsuf, dsuf[v]);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size(), i;
+        vector<int> dsuf(n + 1, -1);
+
+        for (auto edge : edges) {
+            int parent_x = find(dsuf, edge[0]);
+            int parent_y = find(dsuf, edge[1]);
+
+            if (parent_x == parent_y) {
+                return edge;
+            }
+            dsuf[parent_x] = parent_y;
+        }
+        return {0, 0};
+    }
+};
