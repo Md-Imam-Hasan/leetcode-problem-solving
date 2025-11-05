ab81dc3df5ec0f95de505f6d3fe4d9def41c7f70
Md-Imam-Hasan <imamhasan0007@gmail.com>
2025-11-05 15:32:00 +0600

    feat: add new LeetCode solutions

---
+++ LeetCode 75/1679-Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int ans = 0, i = 0, j = nums.size() - 1;
+
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == k) {
+                ans++;
+                i++;
+                j--;
+            } else if (sum > k) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return ans;
+    }
+};
