2ff9ba58a0dc2fdf5f57ef0e6b45877a373140d4
Md-Imam-Hasan <imamhasan0007@gmail.com>
2026-01-11 20:08:42 +0600

    feat: Implement C++ solution for LeetCode 84 Largest Rectangle in Histogram.

--- 84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram.cpp
@@ -1,9 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
-public:
-    int largestRectangleArea(vector<int>& heights) {
-        
-    }
-};
+    public:
+        int largestRectangleArea(vector<int>& heights) {
+            stack<pair<int, int>> st;
+            int maxArea = 0;
+            int n = heights.size();
+    
+            for (int i = 0; i < n; i++) {
+                int start = i;
+    
+                while (!st.empty() && st.top().second > heights[i]) {
+                    auto top = st.top();
+                    maxArea = max(maxArea, top.second * (i - top.first));
+                    start = top.first;
+                    st.pop();
+                }
+                st.push({start, heights[i]});
+            }
+    
+            while (!st.empty()) {
+                auto top = st.top();
+                maxArea = max(maxArea, top.second * (n - top.first));
+                st.pop();
+            }
+    
+            return maxArea;
+        }
+    };
