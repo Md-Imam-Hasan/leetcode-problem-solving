7b009dca9751fb472f6f9d972e36fca6f8eab65c
Md-Imam-Hasan <imamhasan0007@gmail.com>
2026-01-20 13:16:17 +0600

    feat: Add C++ solution for LeetCode problem on constructing the minimum bitwise array.

---
+++ DCC January 2026/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+      vector<int> minBitwiseArray(vector<int>& nums) {
+          vector<int> ans;
+  
+          for(int i = 0;i<nums.size();i++){
+              bool good = false;
+              for(int j = 0;j<nums[i];j++){
+                  if((j|(j+1)) == nums[i]){
+                      good = true;
+                      ans.push_back(j);
+                      break;
+                  }
+              }
+              if(!good){
+                  ans.push_back(-1);
+              }
+          }
+  
+          return ans;
+      }
+  };
